26a5e1504b13f34f843227e35510e572aed1a35b
LeonardoMRib <108944988+LeonardoMRib@users.noreply.github.com>
2024-04-17 08:55:28 -0300

    17/04

--- script/script.js
+++ script/script.js
@@ -17,6 +17,10 @@ function abrir(num) {
     if(num == "3"){
         document.getElementById("aba3").disabled = true;
         document.getElementById("a3").style.display = "block";
+        pegar_horas();
+        document.getElementById("hd").value = hd;
+        document.getElementById("md").value = md;
+        document.getElementById("sd").value = sd;
     }
     if(num == "4"){
         document.getElementById("aba4").disabled = true;
@@ -73,5 +77,27 @@ function iniciarContagem(){
         contador();
     }, 1000);
 }
-
-
+let hd = 0;
+let md = 0;
+let sd = 0;
+let alarme = null;
+function pegar_horas(){
+    let d = new Date();
+    hd = d.getHours();
+    md = d.getMinutes();
+    sd = d.getSeconds();
+}
+function ativar_alarme(){
+    let d = new Date();
+    if(hd==d.getHours() && md==d.getMinutes() && sd==d.getSeconds()){
+        clearInterval(alarme);
+        alarme("alarme... voltando para aba3");
+        abrir(3);
+    }
+}
+function ligar_alarme(){
+    hd = document.getElementById("hd").value;
+    md = document.getElementById("md").value;
+    sd = document.getElementById("sd").value;
+    alarme = setInterval(ativar_alarme,1000);
+}
